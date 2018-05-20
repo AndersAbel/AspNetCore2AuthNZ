09159e0f7988d529f3444d3432b03ef7867c9c40
Anders Abel <anders.abel@sustainsys.com>
2018-05-20 12:07:58 +0200

    Add project files.

---
+++ AspNetCore2AuthNZ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore2AuthNZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
