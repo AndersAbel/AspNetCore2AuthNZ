84ca37b28b40d06c6e9326c7967d84d236e5258e
Anders Abel <anders.abel@sustainsys.com>
2018-05-21 22:29:32 +0200

    Cart controller and js to call it

---
+++ AspNetCore2AuthNZ/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore2AuthNZ.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(int product)
+        {
+            return Json(17);
+        }
+    }
+}
--- AspNetCore2AuthNZ/wwwroot/js/site.js
+++ AspNetCore2AuthNZ/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿// Write your JavaScript code.
+﻿$('.add-to-cart').click(function () {
+    var data =
+    {
+        product: $(this).attr('data-product')
+    }
+    $.post('/Cart/Add/', data, function (data, status) {
+        $('#cart-count').text(data);
+    });
+})
