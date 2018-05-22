44edf3a10b4fdc4fb991e269ae869d11a35dfcbb
Anders Abel <anders.abel@sustainsys.com>
2018-05-22 19:40:12 +0200

    Login from js client

--- AspNetCore2AuthNZ/wwwroot/js/site.js
+++ AspNetCore2AuthNZ/wwwroot/js/site.js
@@ -11,3 +11,19 @@
 $('.linked-item').click(function () {
     window.location = $(this).attr('data-link');
 });
+
+var config = {
+    authority: "https://localhost:44380",
+    client_id: "js",
+    redirect_uri: "https://localhost:44334/callback.html",
+    response_type: "id_token token",
+    scope: "openid cart"
+};
+
+var mgr = new Oidc.UserManager(config);
+
+mgr.getUser().then(function (user) {
+    if (!user) {
+        mgr.signinRedirect();
+    }
+})
