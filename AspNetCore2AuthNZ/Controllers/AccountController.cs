b37bbc0fc7f0b9fb892ab48aa20d8592347d86a3
Anders Abel <anders.abel@sustainsys.com>
2018-05-23 09:57:38 +0200

    Add access denied page

---
+++ AspNetCore2AuthNZ/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore2AuthNZ.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
